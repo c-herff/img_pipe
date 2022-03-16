538ea285f809862d5f2d8d130d04e9d621497988
Sophocles Goulis <sof_goulis@hotmail.com>
2022-03-16 15:57:01 +0100

    Included label_elecs_hip command for labelling of hippocampal electrodes based on hippocampal segmentation.

--- img_pipe/img_pipe.py
+++ img_pipe/img_pipe.py
@@ -1520,6 +1520,255 @@ class freeCoG:
                                                                                 'eleclabels': elecmontage})
 
         return elec_labels
+        
+    def label_elecs_hip(self, elecfile_prefix='elecs_all', atlas_surf='desikan-killiany', atlas_depth='destrieux', elecs_all=True):
+        ''' Automatically labels electrodes similarly to label_elecs program.
+        For electrodes in the hippocampus, it uses the annotation files created by Freesurfer's hippocampal segmentation program.
+        Produces 4 columns of output, corresponding to HBT, FS60, CA, and full setups.
+        More information can be found here https://surfer.nmr.mgh.harvard.edu/fswiki/HippocampalSubfieldsAndNucleiOfAmygdala
+
+        Important: 'subfield_scan' needs to be defined while initializing the patient.
+        Assumes elecs_all.mat in elecs folder.
+        Saves a new file with _hip.mat in the end.
+        Parameters
+        ----------
+        elecfile_prefix : str, optional
+            prefix of the .mat file with the electrode coordinates matrix
+        atlas_surf : {'desikan-killiany', 'destrieux'}
+            The atlas to use for labeling of surface electrodes.
+        atlas_depth : {'destrieux', 'desikan-killiany'}
+            The atlas to use for labeling of depth electrodes.
+        elecs_all : bool
+            Label all electrodes
+        
+        Returns
+        -------
+        elec_labels : array-like
+            [nchans x 4] matrix of electrode labels. Columns include short name,
+            long name, 'grid'/'depth'/'strip' label, and assigned anatomical label.
+        '''
+
+        if atlas_surf == 'desikan-killiany':
+            surf_atlas_flag = ''
+        elif atlas_surf == 'destrieux':
+            surf_atlas_flag = '--a2009s'
+        else:
+            surf_atlas_flag = ''
+
+        print(self.subj_dir)
+        print('Creating labels from the freesurfer annotation file for use in automated electrode labeling')
+        gyri_labels_dir = os.path.join(self.subj_dir, self.subj, 'label', 'gyri')
+        if not os.path.isdir(gyri_labels_dir):
+            os.mkdir(gyri_labels_dir)
+         
+        # This version of mri_annotation2label uses the coarse labels from the Desikan-Killiany Atlas, unless
+        # atlas_surf is 'destrieux', in which case the more detailed labels are used
+        os.system('mri_annotation2label --subject %s --hemi %s --surface pial %s --outdir %s'%(self.subj, self.hem, surf_atlas_flag, gyri_labels_dir))
+        print('Loading electrode matrix')
+        elecfile = os.path.join(self.elecs_dir, elecfile_prefix+'.mat')
+        elecmatrix = scipy.io.loadmat(elecfile)['elecmatrix']
+        
+        # Initialize empty variable for indices of grid and strip electrodes
+        isnotdepth = []
+        
+        # Choose only the surface or grid electrodes (if not using hd_grid.mat)
+        if elecfile_prefix == 'TDT_elecs_all' or elecfile_prefix == 'clinical_elecs_all' or elecs_all:
+            elecmontage = scipy.io.loadmat(elecfile)['eleclabels']
+            # Make the cell array into something more usable by python
+            short_label = []
+            long_label = []
+            grid_or_depth = []
+ 
+            for r in elecmontage:
+                short_label.append(r[0][0]) # This is the shortened electrode montage label
+                long_label.append(r[1][0]) # This is the long form electrode montage label
+                grid_or_depth.append(r[2][0]) # This is the label for grid, depth, or strip
+            
+            # These are the indices that won't be used for labeling
+            #dont_label = ['EOG','ECG','ROC','LOC','EEG','EKG','NaN','EMG','scalpEEG']
+            indices = [i for i, x in enumerate(long_label) if ('EOG' in x or 'ECG' in x or 'ROC' in x or 'LOC' in x or 'EEG' in x or 'EKG' in x or 'NaN' in x or 'EMG' in x or x==np.nan or 'scalpEEG' in x)]
+            indices.extend([i for i, x in enumerate(short_label) if ('EOG' in x or 'ECG' in x or 'ROC' in x or 'LOC' in x or 'EEG' in x or 'EKG' in x or 'NaN' in x or 'EMG' in x or x==np.nan or 'scalpEEG' in x)])
+            indices.extend([i for i, x in enumerate(grid_or_depth) if ('EOG' in x or 'ECG' in x or 'ROC' in x or 'LOC' in x or 'EEG' in x or 'EKG' in x or 'NaN' in x or 'EMG' in x or x==np.nan or 'scalpEEG' in x)])
+            indices.extend(np.where(np.isnan(elecmatrix)==True)[0]) 
+            indices = list(set(indices))
+            indices_to_use = list(set(range(len(long_label))) - set(indices))
+
+            # Initialize the cell array that we'll store electrode labels in later
+            elec_labels_orig = np.empty((len(long_label),7),dtype=np.object)
+            elec_labels_orig[:,0] = short_label
+            elec_labels_orig[:,1] = long_label
+            elec_labels_orig[:,2] = grid_or_depth 
+
+            elec_labels = np.empty((len(indices_to_use),7), dtype = np.object)
+            elecmatrix_orig = elecmatrix
+            elecmatrix = elecmatrix[indices_to_use,:]
+            
+            short_label_orig,long_label_orig,grid_or_depth_orig = short_label,long_label,grid_or_depth
+            short_label = [i for j, i in enumerate(short_label) if j not in indices]
+            long_label = [i for j, i in enumerate(long_label) if j not in indices]
+            grid_or_depth = [i for j, i in enumerate(grid_or_depth) if j not in indices]
+            elec_labels[:,0] = short_label
+            elec_labels[:,1] = long_label
+            elec_labels[:,2] = grid_or_depth
+            
+            # Find the non depth electrodes
+            isnotdepth = np.array([r!='depth' for r in grid_or_depth])
+            
+        # Use the surface label files to get which label goes with each surface vertex
+        label_files = glob.glob(os.path.join(gyri_labels_dir, '%s.*.label'%(self.hem)))
+        vert_label = {}
+        for label in label_files:
+            label_name = label.split('.')[1]
+            print('Loading label %s'%label_name)
+            fid = open(label,'r')
+            d = np.genfromtxt(fid, delimiter=' ', \
+                              skip_header=2)
+            vertnum, x, y, z, junk=d[~np.isnan(d)].reshape((-1,5)).T
+            for v in vertnum:
+                vert_label[np.int(v)] = label_name.strip()
+            fid.close()
+
+        trivert_file = os.path.join(self.mesh_dir, '%s_pial_trivert.mat'%(self.hem))
+        cortex_verts = scipy.io.loadmat(trivert_file)['vert']
+
+        # Only use electrodes that are grid or strips
+        if len(isnotdepth)>0:
+            elecmatrix_new = elecmatrix[isnotdepth,:]
+        else:
+            elecmatrix_new = elecmatrix
+
+        print('Finding nearest mesh vertex for each electrode')
+        vert_inds, nearest_verts = self.nearest_electrode_vert(cortex_verts, elecmatrix_new)
+
+        ## Now make a dictionary of the label for each electrode
+        elec_labels_notdepth=[]
+        for v in range(len(vert_inds)):
+            if vert_inds[v] in vert_label:
+                elec_labels_notdepth.append(vert_label[vert_inds[v]].strip())
+            else:
+                elec_labels_notdepth.append('Unknown')
+
+        if elecfile_prefix == 'TDT_elecs_all' or elecfile_prefix == 'clinical_elecs_all' or elecs_all:
+            elec_labels[isnotdepth,3:7] = np.matlib.repmat(elec_labels_notdepth,1,4).T
+            elec_labels[np.invert(isnotdepth),3:7] = '' # Set these to an empty string instead of None type
+        else:
+            elec_labels = np.array(elec_labels_notdepth, dtype = np.object)
+        print('Saving electrode labels for surface electrodes to %s'%(elecfile_prefix))
+
+
+        ## added by BKD so that elec_mat_grid='hd_grid' works. It does not contain elecmontage
+        save_dict = {'elecmatrix': elecmatrix, 'anatomy': elec_labels}
+        if 'elecmontage' in locals():
+            save_dict['eleclabels'] = elecmontage
+        else:
+            print('electmontage does not exist')
+        #scipy.io.savemat('%s/%s/elecs/%s'%(self.subj_dir, self.subj, elecfile_prefix), save_dict)
+
+        if np.any(np.invert(isnotdepth)): # If there are depth electrodes, run this part
+            print('*************************************************')
+            print('Now doing the depth electrodes')
+
+            # Get the volume corresponding to the labels from the Destrieux atlas, which is more 
+            # detailed than Desikan-Killiany (https://surfer.nmr.mgh.harvard.edu/fswiki/CorticalParcellation)
+            if atlas_depth == 'desikan-killiany':
+                depth_atlas_nm = ''
+            elif atlas_depth == 'destrieux':
+                depth_atlas_nm = '.a2009s'
+            else:
+                depth_atlas_nm = '.a2009s'
+
+            aseg_file = os.path.join(self.subj_dir, self.subj, 'mri', 'aparc%s+aseg.mgz'%(depth_atlas_nm))
+            dat = nib.freesurfer.load(aseg_file)
+            aparc_dat = dat.get_data()
+             
+            # Define the affine transform to go from surface coordinates to volume coordinates (as CRS, which is
+            # the slice *number* as x,y,z in the 3D volume. That is, if there are 256 x 256 x 256 voxels, the
+            # CRS coordinate will go from 0 to 255.)
+            affine = np.array([[  -1.,    0.,    0.,  128.],
+                               [   0.,    0.,    1., -128.],
+                               [   0.,   -1.,    0.,  128.],
+                               [   0.,    0.,    0.,    1.]])
+
+            elecs_depths = elecmatrix[np.invert(isnotdepth),:]
+            intercept = np.ones(len(elecs_depths))
+            elecs_ones = np.column_stack((elecs_depths,intercept))
+
+            # Find voxel CRS
+            VoxCRS = np.dot(np.linalg.inv(affine), elecs_ones.transpose()).transpose().astype(int)
+            # Make meshgrid the same size as aparc_dat (only for gaussian blob version), ignore
+            #xx, yy, zz = np.mgrid[0:aparc_dat.shape[0], 0:aparc_dat.shape[1], 0:aparc_dat.shape[2]]
+            #unique_labels = np.unique(aparc_dat)
+            #unique_labels = unique_labels[unique_labels>0]
+
+            # Get the names of these labels using Freesurfer's lookup table (LUT)
+            print("Loading lookup table for freesurfer labels")
+            fid = open(os.path.join(self.fs_dir,'FreeSurferColorLUT.txt'))
+            LUT = fid.readlines()
+            fid.close()
+
+            # Make dictionary of labels
+            LUT = [row.split() for row in LUT]
+            lab = {}
+            for row in LUT:
+                if len(row)>1 and row[0][0]!='#' and row[0][0]!='\\': # Get rid of the comments
+                    lname = row[1]
+                    lab[np.int(row[0])] = lname
+
+            # Label the electrodes according to the aseg volume
+            nchans = VoxCRS.shape[0]
+            anatomy = np.empty((nchans,), dtype=np.object)
+            anatomyRH = np.empty((nchans,), dtype=np.object)
+            anatomyLH = np.empty((nchans,), dtype=np.object)
+            anatomySF = np.empty((nchans,), dtype=np.object)
+
+            #If we are including subfields...
+            if(self.subfield_scan != 'None'):
+                print("Labeling electrodes including subfields...")
+                atlases=['.HBT.','.FS60.','.CA.','.']
+                for i,atl in enumerate(atlases):
+                    aseg_file = os.path.join(self.subj_dir, self.subj, 'mri', 'lh.hippoAmygLabels-' + self.subfield_scan + '.v21' + atl + 'FSvoxelSpace.mgz')
+                    dat = nib.freesurfer.load(aseg_file)
+                    aparc_dat_lh = dat.get_data()
+                
+                    aseg_file = os.path.join(self.subj_dir, self.subj, 'mri', 'rh.hippoAmygLabels-' + self.subfield_scan + '.v21' + atl + 'FSvoxelSpace.mgz')
+                    dat = nib.freesurfer.load(aseg_file)
+                    aparc_dat_rh = dat.get_data()
+
+                    for elec in np.arange(nchans):
+                        if(lab[aparc_dat_rh[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]] != 'Unknown'):
+                            anatomy[elec] = 'Right-' + lab[aparc_dat_rh[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]]
+
+                        elif(lab[aparc_dat_lh[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]] != 'Unknown'):
+                            anatomy[elec] = 'Left-' + lab[aparc_dat_lh[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]]
+
+                        else:
+                            anatomy[elec] = lab[aparc_dat[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]]
+                        print("E%d, Vox CRS: [%d, %d, %d], Label #%d = %s"%(elec, VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2], 
+                                                                        aparc_dat[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]], 
+                                                                        anatomy[elec]))
+                    
+                    elec_labels[np.invert(isnotdepth),3+i] = anatomy
+                    print("Finished labelling with the %s set"%(atl))
+                    
+            #If we are not including subfields
+            else:
+                print("Labeling electrodes...")
+                for elec in np.arange(nchans):
+                    anatomy[elec] = lab[aparc_dat[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]]]
+                    print("E%d, Vox CRS: [%d, %d, %d], Label #%d = %s"%(elec, VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2], 
+                                                                aparc_dat[VoxCRS[elec,0], VoxCRS[elec,1], VoxCRS[elec,2]], 
+                                                                anatomy[elec]))
+                elec_labels[np.invert(isnotdepth),3] = anatomy
+
+        #make some corrections b/c of NaNs in elecmatrix
+        elec_labels_orig[:,3:7] = ''
+        elec_labels_orig[indices_to_use,3:7] = elec_labels[:,3:7] 
+        
+        print('Saving electrode labels to %s'%(elecfile_prefix))
+        scipy.io.savemat(os.path.join(self.elecs_dir, elecfile_prefix+'_hip.mat'), {'elecmatrix': elecmatrix_orig, 
+                                                                                'anatomy': elec_labels_orig})
+
+        return elec_labels
 
     def warp_all(self, elecfile_prefix='TDT_elecs_all', warp_depths=True, warp_surface=True, template='cvs_avg35_inMNI152'):
         ''' Warps surface and depth electrodes and runs quality checking functions for them. 
